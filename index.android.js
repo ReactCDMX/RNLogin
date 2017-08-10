8633c3e56a16f5f9be2fef44a7c60743c30c2a8f
Lepoldo Caballero <git@pitakill.net>
2017-08-09 20:01:12 -0500

    Login w/Facebook and Firebase

--- index.android.js
+++ index.android.js
@@ -9,10 +9,25 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  TouchableOpacity
+  TouchableOpacity,
+  View
 } from 'react-native';
-import {LoginManager} from 'react-native-fbsdk';
+import {
+  AccessToken,
+  LoginManager
+} from 'react-native-fbsdk';
+import RNFirebase from 'react-native-firebase';
+
+const config = {
+  apiKey: "AIzaSyCnfbQ2Is1hODGbF_nKGCCyzBVSchIy95o",
+  authDomain: "rnlogin-822e7.firebaseapp.com",
+  databaseURL: "https://rnlogin-822e7.firebaseio.com",
+  projectId: "rnlogin-822e7",
+  storageBucket: "rnlogin-822e7.appspot.com",
+  messagingSenderId: "818148562943"
+};
+
+const firebase = RNFirebase.initializeApp(config);
 
 export default class RNLogin extends Component {
   _fbAuth() {
@@ -20,17 +35,24 @@ export default class RNLogin extends Component {
       .logInWithReadPermissions(['public_profile', 'email'])
       .then(result => {
         if (result.isCancelled) {
-          console.log('Login was Cancelled');
+          return Promise.reject('The user cancelled the login with Facebook');
         } else {
-          console.log(`Login was a success ${result.grantedPermissions.toString()}`)
+          return AccessToken.getCurrentAccessToken();
         }
-      }, error => consle.error(error))
+      })
+      .then(({accessToken}) => {
+        const credential = firebase.auth.FacebookAuthProvider.credential(accessToken);
+        return firebase.auth().signInWithCredential(credential);
+      })
+      .then(currentUser => console.info(JSON.stringify(currentUser.toJSON())))
+      .catch(error => console.error(error));
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this._fbAuth()}>
+        <TouchableOpacity onPress={this._fbAuth}>
+          <Text>Login with Facebook</Text>
         </TouchableOpacity>
       </View>
     );
